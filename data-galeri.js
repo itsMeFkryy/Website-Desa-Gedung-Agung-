1e40296d058bf4fc545efec045f6933e4f3cbcd1
fkrryy <alfikrideo1525@gmail.com>
2026-02-06 08:14:47 +0700

    Add files via upload

---
+++ data-galeri.js
@@ -0,0 +1,77 @@
+const dataGaleri = [
+    {
+        src: "img/posyandu4.jpg",
+        title: "Kegiatan Rutin Posyandu Dusun III",
+        category: "kegiatan"
+    },
+    {
+        src: "img/senamSore.jpg",
+        title: "Senam Sore Ibu-Ibu",
+        category: "kegiatan"
+    },
+    {
+        src: "img/bakarJagung.jpg",
+        title: "Pembakaran Bonggol Jagung",
+        category: "potensi"
+    },
+    {
+        src: "img/musyawarah.jpg",
+        title: "Musyawarah Desa",
+        category: "kegiatan"
+    },
+    {
+        src: "img/posyandu.jpg",
+        title: "Kegiatan Rutin Posyandu Dusun II",
+        category: "kegiatan"
+    },
+    {
+        src: "img/umkm.jpg",
+        title: "Kerajinan UMKM",
+        category: "potensi"
+    },
+    {
+        src: "img/posyandu3.jpg",
+        title: "Kegiatan Rutin Posyandu Dusun I",
+        category: "potensi"
+    },
+    {
+        src: "img/sekolah1.jpg",
+        title: "Mewarnai Bersama Siswa",
+        category: "kegiatan"
+    },
+    {
+        src: "img/pengajian1.jpg",
+        title: "Pengajian Rutin",
+        category: "kegiatan"
+    },
+    {
+        src: "img/dusunIII.jpeg",
+        title: "Lingkungan Dusun III",
+        category: "alam"
+    },
+    {
+        src: "img/logistikSapi.jpeg",
+        title: "Pengangkutan Logistik Sapi",
+        category: "potensi"
+    },
+    {
+        src: "img/dusun2.jpeg",
+        title: "Irigasi Sawah Dusun II",
+        category: "potensi"
+    },
+    {
+        src: "img/senamDusun5.jpg",
+        title: "Senam Bersama di Dusun 5",
+        category: "kegiatan"
+    },
+    {
+        src: "img/bimbel.jpg",
+        title: "Bimbingan Belajar Bersama",
+        category: "kegiatan"
+    },
+    {
+        src: "img/upacara.jpg",
+        title: "Upacara Bendera di Sekolah",
+        category: "kegiatan"
+    },
+];
